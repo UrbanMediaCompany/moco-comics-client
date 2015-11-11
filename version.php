acca4ee0788b9973aeee7239871497a436977d12
Diego Islas Ocampo <hyuchia@gmail.com>
2015-11-10 22:44:01 -0600

    Updated Files

--- version.php
+++ version.php
@@ -10,8 +10,8 @@
 class Version {
 
 	function __construct(){
-		$this-> version = "1.2.3";
-		$this-> version_number = "6";
+		$this-> version = "1.3.0";
+		$this-> version_number = "7";
 		$this-> update_url = "http://update.codify.mx/MocoComics/";
 
 	}
