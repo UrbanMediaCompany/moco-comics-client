a77dd0e9f89a24fc021349b62e0c9f23e3d0e536
Diego Islas Ocampo <hyuchia@gmail.com>
2016-03-31 10:52:40 -0600

    Empty old files

--- js/plugins.js
+++
@@ -1,6 +0,0 @@
-jQuery.fn.mailto = function(){
-    return this.each(function(){
-        var email = $(this).attr("href").replace("(at)", "@").replace("(dot)", ".");;
-        $(this).before('<a href="mailto:' + email + '" class="'+$(this).attr("class")+'"  title="Email ' + email + '" id="'+$(this).attr("id")+'">'+$(this).html()+'</a>').remove();
-    });
-};
