aca24dc60766a74c21c9323d0d8cd8fd6f0438a7
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2020-11-13 12:20:48 -0600

    Add global styles

--- packages/client/src/main.js
+++ packages/client/src/main.js
@@ -1,6 +1,10 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 import DefaultLayout from '~/layouts/Default.vue';
+import 'typeface-assistant';
+import 'typeface-paytone-one';
+import 'typeface-luckiest-guy';
+import '~/styles/global.css';
 
 export default function (Vue) {
   // Set default layout as a global component
