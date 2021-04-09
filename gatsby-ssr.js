35caa660e97cc8c87600e643870a4d0bdede4e3a
Luis Fernando Saavedra Meza <dev@fsvdr.me>
2021-04-08 22:30:41 -0500

    Add Plausible script

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,4 +1,18 @@
-import './src/styles/tailwind.css';
+import React from 'react';
+import { Helmet } from 'react-helmet';
 import '@fontsource/assistant';
 import '@fontsource/paytone-one/400.css';
 import '@fontsource/luckiest-guy/400.css';
+import './src/styles/tailwind.css';
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <>
+      <Helmet>
+        <script async defer data-domain="moco-comics.com" src="https://plausible.io/js/plausible.js"></script>
+      </Helmet>
+
+      {element}
+    </>
+  );
+};
