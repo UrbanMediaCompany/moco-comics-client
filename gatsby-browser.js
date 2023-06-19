76648671ca9a3f58e4c1a7ee7d13776fa18960b4
Fernando Saavedra <dev@fsvdr.me>
2023-06-19 10:40:00 -0600

    feat: Add Fathom analytics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,7 +9,7 @@ export const wrapRootElement = ({ element }) => {
   return (
     <>
       <Helmet>
-        <script async defer data-domain="moco-comics.com" src="https://stats.moco-comics.com/js/index.js"></script>
+        <script src="https://cdn.usefathom.com/script.js" data-site="MUJFEEBC" defer></script>
       </Helmet>
 
       {element}
